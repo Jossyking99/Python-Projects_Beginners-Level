c5a3c88f6b6dee722e09f640de25dd964e173751
Jossyking99 <josephakharume97@gmail.com>
2023-12-25 13:24:17 +0000

    files added with questions

---
+++ Quiz_Game/questions.py
@@ -0,0 +1,30 @@
+quiz = {
+    1 : {
+        "question" : "Who created Python programming language?",
+        "answer" : "Guido van Rossum"
+    },
+    2 : {
+        "question" : "In what year was Python language created?",
+        "answer" : "1991"
+    },
+    3 : {
+        "question" : "A real world entity having attribute, behaviour and identity is?",
+        "answer" : "Object"
+    },
+    4 : {
+        "question" : "What data structure is the foundation of a python dictionary or set?",
+        "answer" : "Hash Table"
+    },
+    5 : {
+        "question" : "What is the size of the int data type on 64-bit machine?",
+        "answer" : "4 byte"
+    },
+    6 : {
+        "question" : "How many ways can a code be commentted?",
+        "answer" : "2 ways"
+    },
+    7 : {
+        "question" : "How is Python different from other programming languages?",
+        "answer" : "Its simplicity and readability"
+    },
+}
